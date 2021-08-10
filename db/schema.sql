3a2948501e7bf0b4190e2a3c4bf96d839786ab6e
ncougs <ncougan@gmail.com>
2021-08-10 15:58:35 +1000

    Adding db schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS business;
+CREATE DATABASE business;
+
+USE business;
+
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+DROP TABLE IF EXISTS role;
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+
+
+DESCRIBE department;
+DESCRIBE role;
+DESCRIBE employee;
+
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,38 @@
+INSERT INTO department (name)
+VALUES  ("Finance"),
+        ("Operations"),
+        ("Digital"),
+        ("Marketing"),
+        ("People and Culture");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Head of Finance", 150000, 1),
+        ("Finance Analyst", 100000, 1),
+        ("Finance Associate", 80000, 1),
+        ("Head of Operations", 150000, 2),
+        ("Operations Consultatnt", 100000, 2),
+        ("Operations Associate", 80000, 2),
+        ("Head of Digital", 150000, 3),
+        ("Digital Developer", 100000, 3),
+        ("Content Manager", 80000, 3),
+        ("Head of Marketing", 150000, 4),
+        ("Brand Manager", 100000, 4),
+        ("Content Manager", 80000, 4),
+        ("Head of People and Culture", 150000, 5),
+        ("Employee Wellbeing", 100000, 5),
+        ("Employee Support", 80000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Nicholas", "Cougan", 1, 1),
+        ("Franziska", "Alf", 7, 2),
+        ("Julian", "Cougan", 10, 3);
+
+
+
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
+       
+       
