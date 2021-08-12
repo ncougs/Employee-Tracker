8dd833ce99b1995bdc40cccbd86d19f56fe9ebde
ncougs <ncougan@gmail.com>
2021-08-12 15:11:00 +1000

    Updatein connection to DB by pulling data from .env

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,12 @@
+require('dotenv').config()
+
 const mysql = require('mysql2');
 
 // create the connection to database
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: 'business'
   });
 
